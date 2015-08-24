f5443e8769d78336281466316a937861cbc7eb13
Chapman Ou <ochapman.cn@gmail.com>
2015-08-25 07:47:25 +0800

    Add sample detectface

---
+++ sample/detectface/detectface.go
@@ -0,0 +1,43 @@
+/*
+* File Name:	detectface.go
+* Description:
+* Author:	Chapman Ou <ochapman.cn@gmail.com>
+* Created:	2015-08-25
+ */
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/ochapman/youtu/TencentYoutuyun"
+)
+
+func main() {
+	//Register your app on http://open.youtu.qq.com
+	//Get the following details
+	appID := uint32(1000061)
+	secretID := "AKID4Bhs9vqYT6mHa9TkIrAe7w5oijOCEjql"
+	secretKey := "P2VTKNvTAnYNwBrqXbgxRSFQs6FTEhNJ"
+	userID := "3041722595"
+
+	as, err := youtu.NewAppSign(appID, secretID, secretKey, userID)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "NewAppSign() failed: %s\n", err)
+		return
+	}
+	imgData, err := ioutil.ReadFile("../../testdata/imageA.jpg")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ReadFile() failed: %s\n", err)
+		return
+	}
+
+	yt := youtu.Init(as, youtu.DefaultHost)
+	df, err := yt.DetectFace(imgData, youtu.DetectModeNormal)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "DetectFace() failed: %s", err)
+		return
+	}
+	fmt.Printf("df: %#v\n", df)
+}
